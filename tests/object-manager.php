ff34280d89d60df23c6d5eb9ec6f4300b9af2f36
Gabor Jonas <gaborjonas8813@gmail.com>
2026-05-24 17:04:12 +0100

    Fix CS

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -11,4 +11,5 @@ new Dotenv()->bootEnv(__DIR__ . '/../.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
+
 return $kernel->getContainer()->get('doctrine')->getManager();
